c6debe01b152b3396e0ade6a21bad91636f3234c
Felipe Calderon <eselopio@gmail.com>
2026-02-06 00:40:24 -0300

    feat: performance

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "@vercel/analytics/astro" {
+  import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+  const Analytics: AstroComponentFactory<{ mode?: "auto" | "development" | "production" }>;
+  export default Analytics;
+}
