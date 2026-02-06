35119d7c9998bdca7af690a48805a9ad10e4a534
Felipe Calderon <eselopio@gmail.com>
2026-02-06 01:26:41 -0300

    feat: grafana faro

---
+++ src/utils/faro.ts
@@ -0,0 +1,23 @@
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+export function initFaro() {
+    if (typeof window !== 'undefined') {
+        initializeFaro({
+        url: 'https://faro-collector-prod-sa-east-1.grafana.net/collect/857715f4133863ff696cded10f691b82',
+        app: {
+        name: 'pipa11y-cv',
+        version: '1.0.0',
+        environment: 'production'
+        },
+        
+        instrumentations: [
+        // Mandatory, omits default instrumentations otherwise.
+        ...getWebInstrumentations(),
+    
+        // Tracing package to get end-to-end visibility for HTTP requests.
+        new TracingInstrumentation(),
+        ],
+        });
+    }
+}
